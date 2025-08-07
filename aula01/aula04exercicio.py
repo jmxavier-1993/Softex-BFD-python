23ecf6f6c343ec62f07490bf4a48ad4db0b5b9e4
Juliana Xavier <juliana.marinhoppf5@hotmail.com>
2025-08-07 23:52:02 +0000

    modificacao com outras questoes

--- aula01/aula04exercicio.py
+++ aula01/aula04exercicio.py
@@ -28,3 +28,33 @@ print("#   #")
 print("#####")
 
 print("#####" "\n" "#   #""\n" "#   #""\n" "#####" "\n")
+
+# operacoes basicas
+
+var01=6
+var02=4
+soma= var01+ var02
+print(soma)
+subtracao= var01-var02
+print(subtracao)
+multiplicacao= var01* var02
+print(multiplicacao)
+divisao=var01/var02
+print(divisao)
+restodivisao= var01%var02
+print(restodivisao)
+divisao1=var01//var02
+print(divisao1)
+exponencial=var01**var02
+print(exponencial)
+
+# calculadora simples
+var04=int(input("digite um numero"))
+var05=int(input("digite outro numero"))
+operacao=input("qual operacao deseja fazer [S] ou[D]")
+if (operacao == "S"):
+    print(var04+var05)
+elif(operacao == "D"):
+    print(var04*var05)
+else:
+    print("opção invalida")      
