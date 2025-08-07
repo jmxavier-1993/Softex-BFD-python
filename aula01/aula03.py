c3ae4e5ab90543ccbddae366d80dc745394f7722
Juliana Xavier <juliana.marinhoppf5@hotmail.com>
2025-08-07 23:34:58 +0000

    exercicios

---
+++ aula01/aula03.py
@@ -0,0 +1,37 @@
+"""
+Soma +
+Subtracao -
+Multiplicacao *
+Divisao /
+Resto da divisao %
+Divisao por inteiro //
+Exponencial **
+"""
+
+var01=6
+var02=4
+soma= var01+ var02
+print(soma)
+subtracao= var01-var02
+print(subtracao)
+multiplicacao= var01* var02
+print(multiplicacao)
+divisao=var01/var02
+print(divisao)
+restodivisao= var01%var02
+print(restodivisao)
+divisao1=var01//var02
+print(divisao1)
+exponencial=var01**var02
+print(exponencial)
+"""
+operadores logicos
+== comparacao
+> maior
+< menor
+= atribuicao
+<= menor ou igual 
+>= maior ou igual 
++= atribuicao do valor a variavel citada antes 
+"""
+
---
+++ aula01/aula04exercicio.py
@@ -0,0 +1,30 @@
+# mostre tres frases diferentes usando tres comandos prints separados
+print ("hello word", "Juliana Marinho")
+print ("hello word", "Juliana Marinho")
+print ("hello word", "Juliana Marinho")
+
+# mostre o seguinte  texto formatado:
+# Nome:Joao
+# idade: 20 anos
+#Curso :Python basico
+
+nome="Joao"
+idade= 20
+Curso="Python basico"
+print(nome ,"\n", idade ,"\n", Curso ,"\n")
+
+# combinar textos e numeros
+print(f"Meu nome eh:{nome} Minha idade eh:{idade} Eu faco curso de:{Curso}")
+
+# use print para desenhar um quadrado com ## como:
+# ####
+# #  #
+# #  #
+# ####
+
+print("#####")
+print("#   #")
+print("#   #")
+print("#####")
+
+print("#####" "\n" "#   #""\n" "#   #""\n" "#####" "\n")
