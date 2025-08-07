acef4164d5bdefde64d465aede36d24e0f9d7618
Juliana Xavier <juliana.marinhoppf5@hotmail.com>
2025-08-07 22:41:29 +0000

    aula02

---
+++ aula01/aula02.py
@@ -0,0 +1,23 @@
+variavel01="Juliana"
+print(variavel01)
+print(type(variavel01))
+variavel02= "033"
+print(type(variavel02))
+variavel03= 1
+print(variavel03 + int(variavel02))
+## as variaveis não podem começar com numeros nem podem ser definidas por palavras reservadas.
+valorstring= "22"
+print(valorstring+valorstring)
+print(valorstring* 5)
+
+notaaluno= 7.8
+notaaluno02= 8
+notaaluno02= float(notaaluno02)
+print(type(notaaluno02))
+print(type(notaaluno))
+print(notaaluno+notaaluno02)
+print(int(notaaluno)*valorstring) ## neste caso precisamos converter o float para int
+casa = True 
+print(notaaluno>notaaluno02)
+
+
