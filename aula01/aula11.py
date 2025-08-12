8a981c269d5b654acd08ccbb60dfb7ebaa605d7a
Juliana Xavier <juliana.marinhoppf5@hotmail.com>
2025-08-12 20:42:57 -0300

    exercicios

---
+++ aula01/aula11.py
@@ -0,0 +1,5 @@
+
+while True:
+    x= int(input("Digite um número: "))
+    if x == 0:
+       print("Você encerrou o programa com o número", x)
---
+++ aula14.py
@@ -0,0 +1,8 @@
+num1 = 0
+numeros = []
+while num1 != 1:
+    num1 = int(input("Digite o primeiro número: "))
+    numeros.append(num1)    
+    
+else:
+    print("Você encerrou o programa com a soma de:", sum(numeros))
