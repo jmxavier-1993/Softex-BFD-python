8eddc22e760aea57b2760aadc5e6249661afa928
Juliana Xavier <juliana.marinhoppf5@hotmail.com>
2025-08-14 20:55:24 -0300

    melhor forma da inserção na lista com for

--- aula01/aula17exercicio.py
+++ aula01/aula17exercicio.py
@@ -11,8 +11,12 @@ print(dados2)
 
 # criar uma lista com numeros e multiplicar cada numero por 5
 num=[1,2,3,4,5,6,7,8,9]
-multiplica= [(num[0])*5,(num[1])*5,(num[2])*5,(num[3])*5,(num[4])*5,(num[5])*5,(num[6])*5,(num[7])*5,(num[8])*5] 
-print(multiplica)
+# multiplica= [(num[0])*5,(num[1])*5,(num[2])*5,(num[3])*5,(num[4])*5,(num[5])*5,(num[6])*5,(num[7])*5,(num[8])*5] 
+# print(multiplica)
+vazio = []
+for i in num:
+    vazio.append(i * 5)  # Multiplica cada número por 5 e adiciona à lista
+print(vazio)
 
 # pegue a lista a1 =[1, 2, 3, 4, 5,6] e gere uma nova lista apenas com os valores 4 e 5
 
