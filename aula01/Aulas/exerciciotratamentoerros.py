8cb6ff4cf830b8510a658436551a31d003ebebf8
jmxavier-1993 <juliana.marinhoppf5@hotmail.com>
2025-09-05 00:10:29 -0300

    update

--- aula01/Aulas/exerciciotratamentoerros.py
+++ aula01/Aulas/exerciciotratamentoerros.py
@@ -1,46 +1,46 @@
-# # # Escreva um programa que peça ao usuário para digitar um número. 
-# # # Trate o erro caso ele digite algo que não seja um número inteiro.
+# # Escreva um programa que peça ao usuário para digitar um número. 
+# # Trate o erro caso ele digite algo que não seja um número inteiro.
 
-# # # while True:
-# # #     try :
-# # #      num =int(input("Digite um número:"))
-# # #      break
-# # #     except ValueError :
-# # #        print("Opção invalida, digite novamente um número")   
+while True:
+    try :
+     num =int(input("Digite um número:"))
+     break
+    except ValueError :
+       print("Opção invalida, digite novamente um número")   
 
-# # #Peça ao usuário dois números e tente multiplicá-los. 
-# # # Se o usuário digitar algo inválido, exiba uma mensagem de erro.    
-# # while True:
-# #     try :
-# #      num =int(input("Digite um número:"))
-# #      num2 =int(input("Digite outro número:"))
-# #      print(num*num2)
-# #      break
-# #     except ValueError :
-# #        print("Opção invalida, digite novamente um número")   
+# #Peça ao usuário dois números e tente multiplicá-los. 
+# # Se o usuário digitar algo inválido, exiba uma mensagem de erro.    
+while True:
+    try :
+     num =int(input("Digite um número:"))
+     num2 =int(input("Digite outro número:"))
+     print(num*num2)
+     break
+    except ValueError :
+       print("Opção invalida, digite novamente um número")   
 
-# # # Crie um programa que peça ao usuário um número inteiro. 
-# # # Se a conversão for bem-sucedida, mostre uma mensagem usando o bloco else.    
+# # Crie um programa que peça ao usuário um número inteiro. 
+# # Se a conversão for bem-sucedida, mostre uma mensagem usando o bloco else.    
    
 
-# # try:
-# #     num =int(input("Digite um número:"))
-# # except ValueError:
-# #     print("Opção invalida, digite novamente um número")  
-# # else:
-# #    print(f"Você digitou corretamente {num}")        
+try:
+    num =int(input("Digite um número:"))
+except ValueError:
+    print("Opção invalida, digite novamente um número")  
+else:
+   print(f"Você digitou corretamente {num}")        
 
-# #  Implemente um programa que abra um arquivo chamado dados.txt (não precisa existir). 
-# # Use try e finally para garantir que uma mensagem de "Encerrando programa" seja sempre exibida.  
+#  Implemente um programa que abra um arquivo chamado dados.txt (não precisa existir). 
+# Use try e finally para garantir que uma mensagem de "Encerrando programa" seja sempre exibida.  
 
-# try:
-#  with open (r'aula01\Aulas\dados.txt','r') as arquivo:
-#     conteudo = arquivo.read()
-#     print(conteudo)
-# except FileNotFoundError:
-#     print("Erro: O arquivo 'dados.txt' não foi encontrado no caminho especificado.")   
+try:
+ with open (r'aula01\Aulas\dados.txt','r') as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
+except FileNotFoundError:
+    print("Erro: O arquivo 'dados.txt' não foi encontrado no caminho especificado.")   
 
-#Crie uma função dividir(a, b) que lance (raise) uma exceção ZeroDivisionError se b for igual a zero. 
+# Crie uma função dividir(a, b) que lance (raise) uma exceção ZeroDivisionError se b for igual a zero. 
 # Caso contrário, retorne o resultado da divisão. 
 
 def divisao(a,b):
@@ -57,9 +57,22 @@ except ZeroDivisionError as e:
 #  Crie uma exceção personalizada chamada IdadeInvalidaError. 
 # Depois, crie uma função cadastrar_idade(idade) que lance essa exceção caso a idade seja negativa.
 
-class IdadeInvalidaError :
-   
+# Criando a exceção personalizada
+class IdadeInvalidaError(Exception):
+       def __init__(self, mensagem="A idade não pode ser um número negativo."):
+        self.mensagem = mensagem
+        super().__init__(self.mensagem)
 
- def cadastrar_idade(idade) :
-  idade=input("Digite sua idade")
-  
+# Criando a função para cadastrar a idade
+def cadastrar_idade(idade):
+    
+    if idade < 0:
+        raise IdadeInvalidaError()
+    return f"Idade {idade} cadastrada com sucesso!"
+
+# Exemplo de uso da função com e sem a exceção
+try:
+     print(cadastrar_idade(25))
+     print(cadastrar_idade(0))
+except IdadeInvalidaError as e:
+     print(f"Erro: {e.mensagem}")  
