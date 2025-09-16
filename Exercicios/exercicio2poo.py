0ca60e90f7ed2bda9f87fc4b7a0ef4135d5b38ee
Juliana Xavier <142364762+jmxavier-1993@users.noreply.github.com>
2025-09-16 23:33:21 +0000

    update

--- Exercicios/exercicio2poo.py
+++ Exercicios/exercicio2poo.py
@@ -1,50 +1,50 @@
-# # Na classe, ContaBancaria, converta saldo para uma atributo privado. 
-# # Crie um método setter e um getter para acessar e modificar essa atributo,
-# #  seguindo uma regra lógica (Ex: saldo não pode ser negativo)
+# Na classe, ContaBancaria, converta saldo para uma atributo privado. 
+# Crie um método setter e um getter para acessar e modificar essa atributo,
+#  seguindo uma regra lógica (Ex: saldo não pode ser negativo)
 
-# class ContaBancaria:
-#    # atributos
-#    def __init__(self,titular,conta,saldo=0):
-#       self.titular=titular
-#       self.conta=conta
-#       self.__saldo=saldo
-#    def __str__(self):
-#       return f"Conta n {self.conta} do titular {self.titular} tem {self.__saldo} de saldo "   
-#    def __repr__(self):
-#       return f"ContaBancaria(titular={self.titular!r}, numero_conta={self.conta!r}, saldo={self.__saldo!r})"   
+class ContaBancaria:
+   # atributos
+   def __init__(self,titular,conta,saldo=0):
+      self.titular=titular
+      self.conta=conta
+      self.__saldo=saldo
+   def __str__(self):
+      return f"Conta n {self.conta} do titular {self.titular} tem {self.__saldo} de saldo "   
+   def __repr__(self):
+      return f"ContaBancaria(titular={self.titular!r}, numero_conta={self.conta!r}, saldo={self.__saldo!r})"   
    
    
-#     # Getters
-#    def get_saldo(self):
-#         return self.__saldo
+    # Getters
+   def get_saldo(self):
+        return self.__saldo
     
-#     # Setters
-#    def set_saldo(self, valor):
-#         if valor < 0:
-#             print("Saldo não pode ser negativo")
-#         else:
-#             self.__saldo += valor
-# # metodos
+    # Setters
+   def set_saldo(self, valor):
+        if valor < 0:
+            print("Saldo não pode ser negativo")
+        else:
+            self.__saldo += valor
+# metodos
 
-#    def depositar(self,valor) :
-#        self.__saldo += valor
-#        print(f"Foi depositado {valor} na sua conta")
-#        print(f"Seu saldo atual é: {self.__saldo}")
-#        return self.__saldo
+   def depositar(self,valor) :
+       self.__saldo += valor
+       print(f"Foi depositado {valor} na sua conta")
+       print(f"Seu saldo atual é: {self.__saldo}")
+       return self.__saldo
 
-#    def sacar(self,valor) :
-#        if valor> self.__saldo:
-#           print("Saldo insuficiente")
-#        else:
-#         self.__saldo -= valor
-#         print(f" O valor sacado foi {valor}")
-#         return  f"Seu saldo atual é: {self.__saldo}"
+   def sacar(self,valor) :
+       if valor> self.__saldo:
+          print("Saldo insuficiente")
+       else:
+        self.__saldo -= valor
+        print(f" O valor sacado foi {valor}")
+        return  f"Seu saldo atual é: {self.__saldo}"
    
-# conta01=ContaBancaria("Juliana",2131234,50)   
-# conta01.set_saldo(600)
-# print(conta01.get_saldo())
-# print(conta01.sacar(60))
-# print(conta01.get_saldo())
+conta01=ContaBancaria("Juliana",2131234,50)   
+conta01.set_saldo(600)
+print(conta01.get_saldo())
+print(conta01.sacar(60))
+print(conta01.get_saldo())
 
 
 # Crie uma classe, Pessoa, que tenha os atributos: nome, data de nascimento, cpf, identidade. 
