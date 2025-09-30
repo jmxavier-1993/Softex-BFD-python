83b7a05bc2d505c16a6c61b7bed1aa74515bfda0
Juliana Xavier <142364762+jmxavier-1993@users.noreply.github.com>
2025-09-30 23:37:28 +0000

    update

---
+++ Exercicios/exerciciolambda.py
@@ -0,0 +1,7 @@
+# Dobro dos números (map + lambda)
+
+# Dada a lista [1, 2, 3, 4, 5], use map com 
+# lambda para gerar uma nova lista com o dobro de cada número.
+num=[1, 2, 3, 4, 5]
+dobro=lambda num:num*2
+print(map(dobro))
